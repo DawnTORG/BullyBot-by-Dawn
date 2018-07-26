bb853b3d4d70b07e2e50db7688809b50bd6e03f5
DawnTORG <41655849+DawnTORG@users.noreply.github.com>
2018-07-27 00:47:33 +0200

    Update BullyBot.js

--- BullyBot.js
+++ BullyBot.js
@@ -1,40 +1,45 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.login(token);
+
+const prefix = '!';
+
 client.on('ready', () => {
 	console.log("Bot jetzt angeschaltet\n\n")
 });
 
 client.on('message', message => {
 	
-	const syntax = '!';
 	
 	if(message.author.bot) return;
-	//Alle Fragen/Antworten
 	
-	if(message.content.startsWith(syntax + "pn"));
+	if(message.content.startsWith("!pn"));
 	{
-	mention = message.mentions.users.first();
+		   	mention = message.mentions.users.first();
 	//Die ausgabe an Admin und User
 	if(message.content.startsWith(prefix + "pn")){
 	   if (mention == null) { return; }
            message.delete();
-           mentionMessage = message.content.slice (3);
-           mention.send ('```css\nHey! Dir wurde anonym eine Nachricht geschickt. Das hier ist die Nachricht: \n```' + mentionMessage);
-	   message.author.send ('```css\nHallo! Deine Nachricht wurde anonym an ```' + mention + ' ```css\ngesendet. ```');
+		   mentionMessage = message.content.slice (3);
+		   mention.send ('```css\nHey! Dir wurde anonym eine Nachricht geschickt. Das hier ist die Nachricht: \n```' + mentionMessage);
+		   message.author.send ('```css\nHallo! Deine Nachricht wurde anonym an ```' + mention + ' ```css\ngesendet. ```');
       
-           console.log("Es wurde eine PN von " + message.author + " an " + mention + " \n gesendet. Das ist die Nachricht: " + mentionMessage)
+		   console.log("Es wurde eine PN von " + message.author + " an " + mention + " \n gesendet. Das ist die Nachricht: " + mentionMessage)
 		   
 	}
-	
+			
+	}
+
+	//Alle Fragen/Antworten
 	if(message.content.toLowerCase() ==='ich will lachen')
 		message.reply('```css\nSchau in den Spiegel. Dann wirst du dich tot-lachen```');
 	else if(message.content.toLowerCase() === 'ich bin traurig')
 		message.reply('```css\nhör auf zu heulen```');
-	else if(message.content.toLowerCase() === 'was stinkt')
-		message.reply('```css\ndeine Mutter```');
 	else if(message.content.toLowerCase() === 'ich bin glücklich')
 		message.reply('```css\nSuper. Wer wollte das jetzt wissen?```')
+	else if(message.content.toLowerCase() === 'was stinkt')
+		message.reply('```css\ndeine Mutter```');
 	else if(message.content.toLowerCase() === 'meine mutter stinkt nicht')
 		message.reply('```css\nNa, dann bist du das. Geh Duschen!```');
     else if(message.content.toLowerCase() === 'komm privat'){
@@ -58,15 +63,16 @@ client.on('message', message => {
 		message.reply('```css\nIch bin doch nicht gemein...nur ehrlich```');
 	else if(message.content.toLowerCase() === 'sind die anderen Bots hier auch so gemein')
 		message.reply('```css\nJa die sind genau so langweilig wie du```');
-	else if(message.content.toLowerCase() === 'ist es nachts kälter als drausen')
-		message.reply('```css\nJa aber nur weil es zu dunkel ist um etwas zu hören```');
+	else if(message.content.toLowerCase() === 'ist es nachts kälter als drausen'){
+	    message.reply('```css\nJa aber nur weil es zu dunkel ist um etwas zu hören```');}
+	     
 	
 	//!help 
-	else if(message.content.toLowerCase() === syntax + 'help'){
+	if(message.content.toLowerCase() === prefix + 'help'){
 		message.delete();
 		message.author.send('```css\nAlles worauf der Bot eine Antwort gibt: \n"ich will lachen"\n"ich bin traurig"\n"was stinkt hier so"\n"ich bin glücklich"\n"meine mutter stinkt nicht"\n"komm privat"\n"das sage ich meiner mutter"\n"jetzt reicht es"\n"mir ist langweilig"\n"du bist unlustig"\n"ich bin ein gangster"\n"warum ist dir das so wichtig"\n"wie viel erde ist in einem loch"\n"warum bist du so gemein"\n"ist es nachts kälter als drausen"\n\nWeitere Updates in Arbeit```');
-	        message.author.send('```css\nPN funktion: Du kannst in dem du !pn (eine Person) (die Nachricht) , eingibst anonym eine Nachricht verschicken. ```');
-		message.author.send('```css\n\nAlles muss klein geschriben werden und ohne Sonderzeichen```');
+       	message.author.send('```css\nPN funktion: Du kannst in dem du !pn (eine Person) (die Nachricht) , eingibst anonyme Nachrichten verschicken. ```');   
+	    message.author.send('```css\n\nAlles muss klein geschriben werden und ohne Sonderzeichen```');
 		console.log("\n\nEs wurde die Help-liste Aufgerufen")
 	}
 });
